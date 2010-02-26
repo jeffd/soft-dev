6b3719140b3e0a0c7d6a453be664bd3c51fcb67a
Steven A. Klebanoff <stevek@tank.ccs.neu.edu>
2010-02-26 18:16:28 -0500

    added string to artifiact array

--- game_tests.py
+++ game_tests.py
@@ -4,6 +4,8 @@
 { "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "treasure" : "gold", "value" : 9999999 }], "score" : 30 } } ||| congratulations
 { "congratulations" : { "hoard" : [{ "treasure" : "silver plate", "value" : 85 }, { "frog" : [ ] }], "score" : 30 } } ||| congratulations
 { "congratulations" : { "hoard" : [{ "weapon" : "laser gun", "lethality" : 55 }, { "frog" : [ ] }], "score" : 30 } } ||| congratulations
-{ "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "weapon" : "laser gun", "lethality" : 55 }], "score" : 30 } }
-{ "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "artifact" : "tomb", "description" : ["hello"] }], "score" : 30 } }
-{ "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "artifact" : "tomb", "description" : ["hello","world"] }], "score" : 30 } }
+{ "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "weapon" : "laser gun", "lethality" : 55 }], "score" : 30 } } ||| congratulations
+{ "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "artifact" : "tomb", "description" : ["hello"] }], "score" : 30 } } ||| congratulations
+{ "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "artifact" : "tomb", "description" : ["hello","world"] }], "score" : 30 } } ||| congratulations
+{ "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "artifact" : "tomb", "description" : ["hello","world"] }], "score" : 30 } } ||| congratulations
+{ "congratulations" : { "hoard" : [{ "frog" : [ ] }, { "artifact" : "tomb", "description" : ["hello","world","today"] }], "score" : 30 } } ||| congratulations
