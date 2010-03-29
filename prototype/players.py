860f426b432bb091eb4aa08401b5d9558b7f00b7
Steven A. Klebanoff <stevek@bubbles.ccs.neu.edu>
2010-03-29 18:51:17 -0400

    player uses own castle and current location

--- prototype/players.py
+++ prototype/players.py
@@ -2,7 +2,12 @@
 
 class SimplePlayer:
     
-    def next_move(self, current_location, castle, response):
+    def __init__(self, castle, current_location):
+        self.castle = castle
+        self.current_location = current_location
+        self.castle.add_visited_location(current_location)
+    
+    def next_move(self, response):
         """ Function to determine next move, command to send
         and new location"""
     
@@ -13,15 +18,15 @@ class SimplePlayer:
             # FIXME: fix, make prettier, could be something else
             
             # No exits in dict, probably is an exit
-            castle.add_exit(current_location)
+            self.castle.add_exit(self.current_location)
             
             # FIXME: if we have the frog we're done
             # or maybe we should stop
             print 'Found exit'
-            return '(stop)', current_location, False
+            return '(stop)', False
         
         # We have exits, lets go somewhere we haven't been before
-        unexplored_moves = castle.unexplored_moves(current_location, exits)
+        unexplored_moves = self.castle.unexplored_moves(self.current_location, exits)
         if unexplored_moves == []:
             # No unexplored moves, just use first exit
             # TODO: improve
@@ -30,6 +35,12 @@ class SimplePlayer:
             # Use first unexplored move
             direction_to_move = unexplored_moves[0]
         
+        # Save new current position
+        # FIXME: this assumes we get a new response before actually receiving
+        # the response
+        new_location = self.current_location.next_location(direction_to_move)
+        self.current_location = new_location
+        self.castle.add_visited_location(new_location)
+        
         # Go to first exit
-        return ('(go %s)' % direction_to_move), \
-                current_location.next_location(direction_to_move), True
+        return ('(go %s)' % direction_to_move), True
