f57d264019c902d11f4408b61c035269110bc776
Steven A. Klebanoff <stevek@bumpnjump.ccs.neu.edu>
2010-04-13 16:25:28 -0400

    takes in random variables using s and q to test with game 9

--- player/game_player.py
+++ player/game_player.py
@@ -26,6 +26,10 @@ parser.add_option("-d", "--debug", action='store_true', dest="debug",
                   default=False, help="Should we log DEBUG level information?")
 parser.add_option("-i", "--info", dest="info", action='store_true',
                   default=False, help="Should we log INFO level information?")
+parser.add_option("-s", "--random1", dest="random1", help="Random seed variable \
+                  1", type="int")
+parser.add_option("-q", "--random2", dest="random2", help="Random seed variable \
+                  1", type="int")
 options, args = parser.parse_args()
 
 # Get important options
@@ -42,6 +46,9 @@ else:
         parser.error("Please specify a game location, see --help for details")
     # Setup command to execute program
     process = '%s -r6rs -program %s' % (options.larceny, options.game)
+    
+    if options.random1 and options.random2:
+        process += ' -- outputfile %i %i' % (options.random1, options.random2)
 
 # Setup the player
 player = BreadcrumbPlayer()
